f30061b46a07bdf2de551696198c9f7e44de4434
Vito Di Bari <57833823+vitodibari@users.noreply.github.com>
2025-03-01 08:01:02 +0100

    download script improved

--- data/custom_dataset_nst/download_celeba.py
+++ data/custom_dataset_nst/download_celeba.py
@@ -2,29 +2,38 @@ import kagglehub
 import os
 import shutil
 import random
+from tqdm import tqdm
 
-# Download latest version
-#path = kagglehub.dataset_download("jessicali9530/celeba-dataset")
-# Extract the dataset in a certain quantity of portraits
-path="C:\\Users\\DavideSavoia\\OneDrive - Politecnico di Bari\\1M\\DeepL\\progetto\\Portraits dataset\\img_align_celeba"
-print("Path to dataset files:", path)
-if os.path.exists(path):
-    print("Dataset downloaded successfully.")
-    destination_folder = 'datasets/trainA_portraits'
-    os.makedirs(destination_folder, exist_ok=True)
+full_dataset_path = "./full"
+destination_folder_path = './trainA'
+ds_size = 10000
 
-    images = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+print("Download path set to ", os.path.abspath(full_dataset_path))
+print("Destination folder set to ", os.path.abspath(destination_folder_path))
+
+# Download latest version only if the dataset is not already downloaded
+if not os.path.exists(full_dataset_path):
+    downloaded_path = kagglehub.dataset_download("jessicali9530/celeba-dataset")
+    print("Downloaded path: ", downloaded_path)
+
+    # Move the downloaded dataset to the full_dataset_path
+    if os.path.exists(downloaded_path):
+        shutil.move(downloaded_path, full_dataset_path)
+        print("Dataset downloaded successfully.")
+
+# Extract the dataset
+if os.path.exists(full_dataset_path) and not os.path.exists(destination_folder_path):
+    print(f"Extracting {ds_size} randomly selected images to {destination_folder_path}")
+
+    celeba_imgs_path = os.path.join(full_dataset_path, "img_align_celeba/img_align_celeba")
+
+    os.makedirs(destination_folder_path, exist_ok=True)
+
+    images = [f for f in os.listdir(celeba_imgs_path) if os.path.isfile(os.path.join(celeba_imgs_path, f))]
     random.shuffle(images)
-    images = images[:10000]
+    images = images[:ds_size]  # Use only 10k images for training
 
-    for image in images:
-        shutil.copy(os.path.join(path, image), os.path.join(destination_folder, image))
+    for image in tqdm(images):
+        shutil.copy(os.path.join(celeba_imgs_path, image), os.path.join(destination_folder_path, image))
 
-    print(f"Copied {len(images)} images to {destination_folder}.")
-#Cut up to 10k pictures in folder
-"""destination_folder = 'datasets/trainA_portraits'
-images = [f for f in os.listdir(destination_folder) if os.path.isfile(os.path.join(destination_folder, f))]
-images = images[:10000]
-for image in [f for f in os.listdir(destination_folder) if os.path.isfile(os.path.join(destination_folder, f))]:
-    if image not in images:
-        os.remove(os.path.join(destination_folder, image))"""
+print("Done.")
