75872ee335bdbb3944fc3bf6d1c230ce1f987fd5
Davide Savoia <d.savoia@nextome.com>
2025-03-07 02:46:14 +0100

    added custom n_layers_G

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -40,6 +40,7 @@ class CycleGANModel(BaseModel):
         """
         parser.set_defaults(no_dropout=True)  # default CycleGAN did not use dropout
         if is_train:
+            parser.add_argument('--n_layers_G', type=int, default=6, help='only used if netG==ResnetKVAE')
             parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
             parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
             parser.add_argument('--lambda_identity', type=float, default=0.1, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
@@ -75,9 +76,9 @@ class CycleGANModel(BaseModel):
         # The naming is different from those used in the paper.
         # Code (vs. paper): G_A (G), G_B (F), D_A (D_Y), D_B (D_X)
         self.netG_A = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
-                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids, opt.latent_dim, opt.crop_size)
+                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids, opt.latent_dim, opt.crop_size, opt.n_layers_G)
         self.netG_B = networks.define_G(opt.output_nc, opt.input_nc, opt.ngf, opt.netG, opt.norm,
-                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids, opt.latent_dim, opt.crop_size)
+                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids, opt.latent_dim, opt.crop_size, opt.n_layers_G)
 
         if self.isTrain:  # define discriminators
             self.netD_A = networks.define_D(opt.output_nc, opt.ndf, opt.netD,
