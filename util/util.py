409a88b5cae93b4104be4ec81339962c00169448
Davide Savoia <d.savoia@nextome.com>
2025-03-08 18:23:49 +0100

    testing fixes

--- util/util.py
+++ util/util.py
@@ -54,6 +54,7 @@ def save_image(image_numpy, image_path, aspect_ratio=1.0):
         image_path (str)          -- the path of the image
     """
 
+    print('shape=',image_numpy)
     image_pil = Image.fromarray(image_numpy)
     h, w, _ = image_numpy.shape
 
